d47794ccd362999313e0a014d93bcdc18420ebf5
unknown <sani.mia@brainstation23.com>
2021-03-29 14:41:13 +0600

    Added functionality for login handler with ui templare

--- index.js
+++ index.js
@@ -1,13 +1,107 @@
 Vue.component("login-page", {
-    props:["value"],
-    template:`<h1>{{value}}</h1>`
+  props: {
+      /**
+       * Login api for login handler
+       */
+    loginApi: {
+      type: String,
+      required: true,
+    },
+
+    /**
+     * successUrl for redirect path after successful login
+     */
+    successUrl: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return { email: "", password: "", hasError: false };
+  },
+  watch: {
+
+    /**
+     * Hide error feedback ui on input change if visible
+     */
+    email() {
+      if (this.hasError) {
+        this.hasError = false;
+      }
+    },
+    password() {
+      if (this.hasError) {
+        this.hasError = false;
+      }
+    },
+  },
+
+  methods: {
+    /**
+     * Call login api
+     */
+    loginHandler() {
+      if (this.email && this.password) {
+        fetch(this.loginApi)
+          .then((response) => response.json())
+          .then((data) => {
+            this.hasError = false;
+            if (this.successUrl) {
+                /**
+                 * Redirect to the successUrl path
+                 */
+              window.location.href = this.successUrl;
+              this.password = "";
+              this.email = "";
+            }
+          })
+          .catch((error) => {
+            this.hasError = true;
+            console.error("Login error: " + error);
+          });
+      } else {
+        alert("Email and password are required");
+      }
+    },
+  },
+
+  template: `<div class="global-container">
+  <div class="card shadow login-form">
+      <div class="card-body">
+          <h4 class="card-title text-center">Log in to App</h4>
+          <hr />
+          <div class="card-text mb-2">
+              <div v-if="hasError" class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect
+                  username or password.</div>
+          </div>
+
+          <form @submit.prevent="loginHandler()">
+              <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input placeholder="Email" v-model="email" type="email" class="form-control form-control-sm"
+                      id="email" aria-describedby="emailHelp">
+              </div>
+              <div class="form-group">
+                  <label for="password">Password:</label>
+                  <input v-model="password" type="password" class="form-control form-control-sm" id="password"
+                      placeholder="password">
+              </div>
+              <a href="#" class="mb-1" style="float:right;font-size:12px;">Forgot password?</a>
+              <button :disabled="!email||!password" type="submit" class="btn btn-primary btn-block">Log in</button>
+
+              <div class="sign-up">
+                  Don't have an account? <a href="#">Create One</a>
+              </div>
+          </form>
+      </div>
+  </div>
+</div>`,
 });
 
-
+/**
+ * Mount the vue app
+ */
 new Vue({
-    el: "#app",
-    data: {
-      value: "Login-page component",
-    },
-  
-})
+  el: "#app",
+});
